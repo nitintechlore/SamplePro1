1b27611b6162b8a8eba1aee6f345237529d9103a
nitintechlore <nitin@techlore.co.in>
2024-08-07 14:51:20 +0530

    Intial Code

---
+++ MathCalc.ts
@@ -0,0 +1,28 @@
+//Union Types
+type AlphaNumbers= (number | string);
+
+let AddConcatNumStr:(x:AlphaNumbers,y:AlphaNumbers)=>AlphaNumbers;
+
+AddConcatNumStr=(x:AlphaNumbers,y:AlphaNumbers)=>
+{
+   let result:AlphaNumbers="";
+
+   if(typeof x=="string" && typeof y=="string")
+   {
+       result=x+y;
+   }
+   else if(typeof x=="number" && typeof y=="number")
+   {
+    result=x+y;
+   }
+
+   return result;
+}
+
+console.log("---Add Numbers : 20 and 30 -----");
+console.log(AddConcatNumStr(20,30));
+console.log("--------------------------------");
+
+console.log("---Concat String : Str1 and Str2 -----");
+console.log(AddConcatNumStr("Str1","Str2"));
+console.log("--------------------------------");
---
+++ output/MathCalc.js
@@ -0,0 +1,18 @@
+let AddConcatNumStr;
+AddConcatNumStr = (x, y) => {
+    let result = "";
+    if (typeof x == "string" && typeof y == "string") {
+        result = x + y;
+    }
+    else if (typeof x == "number" && typeof y == "number") {
+        result = x + y;
+    }
+    return result;
+};
+console.log("---Add Numbers : 20 and 30 -----");
+console.log(AddConcatNumStr(20, 30));
+console.log("--------------------------------");
+console.log("---Concat String : Str1 and Str2 -----");
+console.log(AddConcatNumStr("Str1", "Str2"));
+console.log("--------------------------------");
+//# sourceMappingURL=MathCalc.js.map
